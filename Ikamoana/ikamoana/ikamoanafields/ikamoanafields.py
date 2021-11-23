df76001c30040a7ec6d603c37a9ef1c6976ff622
Jules Lehodey <juleslehodey31320@hotmail.fr>
2021-11-23 14:59:07 +0100

    Added wrapper for Taxis + function for Diffusion + Some functions to manipulate Fields.

--- Ikamoana/ikamoana/ikamoanafields/ikamoanafields.py
+++ Ikamoana/ikamoana/ikamoanafields/ikamoanafields.py
@@ -1,3 +1,4 @@
+from datetime import time
 from typing import Tuple, Union, List
 
 import numpy as np
@@ -9,10 +10,10 @@ from ..feedinghabitat import FeedingHabitat
 from .ikamoanafieldsconfigreader import readIkamoanaFieldsXML
 
 
-def convertToField(field : Union[xr.DataArray, xr.Dataset], name="Name_to_convert") :
+def convertToField(field : Union[xr.DataArray, xr.Dataset], name=None) :
 
     if isinstance(field, xr.DataArray) :
-        field = field.to_dataset(name=name if field.name is None else None)
+        field = field.to_dataset(name=name if name is not None else field.name)
 
     return parcels.FieldSet.from_xarray_dataset(
         (field.reindex(lat=list(reversed(field.lat)))
@@ -20,7 +21,55 @@ def convertToField(field : Union[xr.DataArray, xr.Dataset], name="Name_to_conver
         variables=dict([(i,i) for i in field.keys()]),
         dimensions=dict([(i,i) for i in field.dims.keys()]))
 
+def sliceField(field : Union[xr.DataArray, xr.Dataset],
+               time_start: int = None, time_end: int = None,
+               lat_min: int = None, lat_max: int = None,
+               lon_min: int = None, lon_max: int = None) -> Union[xr.DataArray, xr.Dataset] :
+    
+    coords = field.coords
+
+    if (lat_min is not None) :
+        if ((lat_min < 0) or (lat_min >= coords['lat'].data.size)) :
+            raise ValueError("lat_min out of bounds. Min is %d and Max is %d"%(
+                0, coords['lat'].data.size - 1))
+    if (lat_max is not None) :
+        if ((lat_max < 0) or (lat_max >= coords['lat'].data.size)) :
+            raise ValueError("lat_max out of bounds. Min is %d and Max is %d"%(
+                0, coords['lat'].data.size - 1))
+    if (lat_min is not None) and (lat_max is not None) and (lat_min > lat_max) :
+        raise ValueError("lat_min must be <= to lat_max.")
+
+    if (lon_min is not None) :
+        if ((lon_min < 0) or (lon_min >= coords['lon'].data.size)) :
+            raise ValueError("lon_min out of bounds. Min is %d and Max is %d"%(
+                0, coords['lon'].data.size - 1))
+    if (lon_max is not None) :
+        if ((lon_max < 0) or (lon_max >= coords['lon'].data.size)) :
+            raise ValueError("lon_max out of bounds. Min is %d and Max is %d"%(
+                0, coords['lon'].data.size - 1))
+    if (lon_min is not None) and (lon_max is not None) and (lon_min > lon_max) :
+        raise ValueError("lon_min must be <= to lon_max.")
+
+    if (time_start is not None) :
+        if ((time_start < 0) or (time_start >= coords['time'].data.size)) :
+            raise ValueError("time_start out of bounds. Min is %d and Max is %d"%(
+                0, coords['time'].data.size - 1))
+    if (time_end is not None) :
+        if ((time_end < 0) or (time_end >= coords['time'].data.size)) :
+            raise ValueError("time_end out of bounds. Min is %d and Max is %d"%(
+                0, coords['time'].data.size - 1))
+    if (time_start is not None) and (time_end is not None) and (time_start > time_end) :
+        raise ValueError("time_start must be <= to time_end.")
+
+    coord_lat = coords["lat"][lat_min:lat_max+1 if lat_max is not None else None]
+    coord_lon = coords["lon"][lon_min:lon_max+1 if lon_max is not None else None]
+    coord_time = coords["time"][time_start:time_end+1 if time_end is not None else None]
+
+    return field.sel(time=coord_time, lat=coord_lat, lon=coord_lon)
+
 class IkamoanaFields :
+
+# ------------------------- CORE FUNCTIONS ------------------------- #
 
     def __init__(self,
                  xml_fields : str,
@@ -31,7 +80,10 @@ class IkamoanaFields :
 
         self.ikamoana_fields_structure = readIkamoanaFieldsXML(xml_fields)
         self.feeding_habitat_structure = FeedingHabitat(xml_feeding_habitat)
-        self.feeding_habitat = feeding_habitat
+        if (feeding_habitat is None) or isinstance(feeding_habitat,xr.DataArray) :
+            self.feeding_habitat = feeding_habitat
+        else :
+            raise TypeError("feeding_habitat must be a Xarray.DataArray or None. Actual type is : %s"%(str(type(feeding_habitat))))
 
     def vMax(self, length : float) -> float :
         """Return the maximum velocity of a fish with a given length."""
@@ -91,9 +143,8 @@ class IkamoanaFields :
                 dims=('lat', 'lon')
             )
 
-    def gradient(self,
-                 field: xr.DataArray,
-                 landmask: xr.DataArray) -> Tuple[xr.DataArray]:
+    def gradient(self, field: xr.DataArray, landmask: xr.DataArray,
+                 name: str = None) -> Tuple[xr.DataArray]:
 
         """
         Gradient calculation for a Xarray DataArray seapodym-equivalent calculation
@@ -109,7 +160,8 @@ class IkamoanaFields :
         # latitude must be south-north rather than north-south.
 
         flip_lat = field.lat[0] > field.lat[-1]
-        if flip_lat : field = field.reindex(lat=np.flip(field.lat))
+        if flip_lat :
+            field = field.reindex(lat=np.flip(field.lat))
         if landmask.lat[0] > landmask.lat[-1] :
             landmask = landmask.reindex(lat=np.flip(landmask.lat))
 
@@ -172,20 +224,23 @@ class IkamoanaFields :
                 dVdlon[t,lat,0] = (data[t,lat,1] - data[t,lat,0]) / dlon[lat,-1]
                 dVdlon[t,lat,-1] = (data[t,lat,-1] - data[t,lat,-2]) / dlon[lat,-1]
 
+        if flip_lat : field = field = field.reindex(lat=np.flip(field.lat))
+
         return (xr.DataArray(
-                    name = 'd' + field.name + '_dlon',
+                    name = "Gradient_longitude_"+(field.name if name is None else name),
                     data = dVdlon,
                     coords = field.coords,
                     dims=('time','lat','lon'),
                     attrs=field.attrs),
                 xr.DataArray(
-                    name = 'd' + field.name + '_dlat',
+                    name = "Gradient_latitude_"+(field.name if name is None else name),
                     data = np.flip(dVdlat, axis=1) if flip_lat else dVdlat,
                     coords = field.coords,
                     dims=('time','lat','lon'),
                     attrs=field.attrs))
 
-    def taxis(self, dHdlon: xr.DataArray, dHdlat: xr.DataArray) -> Tuple[xr.DataArray,xr.DataArray] : 
+    def taxis(self, dHdlon: xr.DataArray, dHdlat: xr.DataArray,
+              name: str = None) -> Tuple[xr.DataArray,xr.DataArray] : 
         """
         Calculation of the Taxis field from the gradient.
         """
@@ -228,13 +283,133 @@ class IkamoanaFields :
         ## NOTE :       (timestep/1852) * (1000*1.852*60) * 1/timestep
         #           <=> (250*1.852*15) * (16/1852)
 
-        return (xr.DataArray(name = 'Tlon',
+        return (xr.DataArray(name = "Taxis_longitude_"+(dHdlon.name if name is None else name),
                              data = Tlon,
                              coords = dHdlon.coords,
                              dims=('time','lat','lon'),
                              attrs=dHdlon.attrs),
-                xr.DataArray(name = 'Tlat',
+                xr.DataArray(name = "Taxis_longitude_"+(dHdlat.name if name is None else name),
                              data = Tlat,
                              coords = dHdlat.coords,
                              dims=('time','lat','lon'),
                              attrs=dHdlat.attrs))
+
+    def diffusion(self, habitat: xr.DataArray, name: str = None) -> xr.DataArray :
+        """DESCRIPTION : This is simply calculating the required indices of the
+        forcing for this simulation."""
+
+        def argumentCheck(array) :
+            if array.attrs.get('cohort_start') is not None :
+                is_evolving = True
+                age = array.cohorts
+            elif array.attrs.get('Cohort number') is not None :
+                is_evolving = False
+                age = array.attrs.get('Cohort number')
+            else :
+                raise ValueError("Fields must contain either 'cohort_start' or 'Cohort number'")
+            return is_evolving, age
+
+        is_evolving, age = argumentCheck(habitat)
+        timestep = self.ikamoana_fields_structure.timestep
+
+        end = habitat.time.size
+        ## TODO : How do we manage NaN values ?
+        # Hdata = habitat.data
+        Hdata = np.nan_to_num(habitat.data)
+        K = np.zeros_like(Hdata)
+        f_length = self.feeding_habitat_structure.data_structure.findLengthByCohort
+
+        for t in range(end):
+
+            t_age = age[t] if is_evolving else age
+            t_length = f_length(t_age) / 100 # Convert into meter
+
+            if self.ikamoana_fields_structure.units == 'nm_per_timestep':
+                Dmax = (t_length*(timestep/1852))**2 / 4
+            else:
+                Dmax = (t_length**2 / 4) * timestep
+            sig_D = self.ikamoana_fields_structure.sigma * Dmax
+
+            ## VECTORIZED
+            K[t,:,:] = (
+                self.ikamoana_fields_structure.sig_scale
+                * sig_D
+                * (1 - self.ikamoana_fields_structure.c_scale
+                    * self.ikamoana_fields_structure.c
+                    * np.power(Hdata[t,:,:], self.ikamoana_fields_structure.P))
+                * self.ikamoana_fields_structure.diffusion_scale
+                + self.ikamoana_fields_structure.diffusion_boost
+            )
+             
+
+        return xr.DataArray(data=K,
+                            name="K_" + (habitat.name if name is None else name),
+                            coords=habitat.coords,
+                            dims=("time","lat","lon"),
+                            attrs=habitat.attrs)
+
+# ------------------------- WRAPPER ------------------------- #
+
+## TODO : Prise en compte des limites dans la structure de données ?
+# Manuel ou automatique ?
+    def computeTaxis(
+            self,
+            cohort: int = None,
+            time_start: int = None, time_end: int = None,
+            lat_min: int = None, lat_max: int = None,
+            lon_min: int = None, lon_max: int = None,
+            name: str = None, use_already_computed_habitat: bool = True,
+            verbose: bool = False) -> Tuple[xr.DataArray,xr.DataArray] :
+        """Description
+        See Also:
+        ---------
+            FeedingHabitat.computeFeedingHabitat
+        """
+
+        if (self.feeding_habitat is None) or (not use_already_computed_habitat) :
+            if cohort is None :
+                raise ValueError("cohort argument must be specified. Actual is %s."%(str(cohort)))
+            feeding_habitat = self.feeding_habitat_structure.computeFeedingHabitat(
+                cohort,time_start,time_end,lat_min,lat_max,lon_min,lon_max,verbose)
+            fh_name = list(feeding_habitat.var()).pop()
+            feeding_habitat = feeding_habitat[fh_name]
+        else :
+            feeding_habitat = self.feeding_habitat
+
+        landmask = self.landmask(
+            habitat_field=(feeding_habitat
+                if self.ikamoana_fields_structure.landmask_from_habitat else None),
+            shallow_sea_to_ocean=self.ikamoana_fields_structure.shallow_sea_to_ocean)
+
+        grad_lon, grad_lat = self.gradient(feeding_habitat, landmask)
+
+        return self.taxis(grad_lon, grad_lat, name=feeding_habitat.name if name is None else name)
+
+    def computeEvolvingTaxis(
+            self,
+            cohort_start: int = None, cohort_end: int = None,
+            time_start: int = None, time_end: int = None,
+            lat_min: int = None, lat_max: int = None,
+            lon_min: int = None, lon_max: int = None,
+            name: str = None, use_already_computed_habitat: bool = True,
+            verbose: bool = False) -> Tuple[xr.DataArray,xr.DataArray] :
+        """Description
+        See Also:
+        ---------
+            FeedingHabitat.computeEvolvingFeedingHabitat
+        """
+        # TODO : Use verbose ?
+        if (self.feeding_habitat is None) or (not use_already_computed_habitat) :
+            feeding_habitat = self.feeding_habitat_structure.computeEvolvingFeedingHabitat(
+                cohort_start,cohort_end,time_start,time_end,lat_min,lat_max,lon_min,lon_max,verbose)
+        else :
+            feeding_habitat = self.feeding_habitat
+            
+        landmask = self.landmask(
+            habitat_field=(feeding_habitat
+                if self.ikamoana_fields_structure.landmask_from_habitat else None),
+            shallow_sea_to_ocean=self.ikamoana_fields_structure.shallow_sea_to_ocean)
+        
+        grad_lon, grad_lat = self.gradient(feeding_habitat, landmask)
+        
+        return self.taxis(grad_lon, grad_lat, name=feeding_habitat.name if name is None else name)
